0cbd0c2c41c68197e42d0f27e06637ae2ce3745d
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-06-02 18:59:51 +0100

    carousel and maps added

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
